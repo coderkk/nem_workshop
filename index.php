b1c44ea86e4cdfbc929f90400f9fb1474a09887e
yhlai <yhlai@digiheritage.com.my>
2019-08-20 07:47:04 +0800

    Correct the framework name from freefat to fatfree

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 // Reference: https://avenir.ro/fat-free-framework-tutorials/fat-free-framework-routes-configuration-file/
 // Kickstart the framework
-$f3=require('lib/freefat/base.php');
+$f3=require('lib/fatfree/base.php');
 
 $f3->set('DEBUG', 3);
 if ((float)PCRE_VERSION<7.9)
